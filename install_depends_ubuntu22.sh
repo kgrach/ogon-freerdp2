5569727d1153b4c08bf6e3d2d65657cad254f643
Kirill Grachev <pllayer@mail.ru>
2024-10-23 12:29:07 +0700

    Add usefull scripts

---
+++ install_depends_ubuntu22.sh
@@ -0,0 +1,3 @@
+sudo apt install xsltproc libssl-dev libx11-dev libxext-dev libxinerama-dev libxcursor-dev \
+		 libxdamage-dev libxv-dev libxkbfile-dev libasound2-dev libcups2-dev libxml2 libxml2-dev \
+		 libxrandr-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libxi-dev libgstreamer-plugins-base1.0-dev
