7418ac17750a3955515417d2f6cc4bb8e70fe2a2
Kirill Grachev <pllayer@mail.ru>
2024-10-25 15:09:02 +0700

    Upd

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 cmake -DCMAKE_BUILD_TYPE=Debug -DWITH_SERVER=ON -DCMAKE_INSTALL_PREFIX=/opt/ogon -DCMAKE_PREFIX_PATH=/opt/ogon/ -DWITH_GSTREAMER_1_0=OFF  .
 make -j $(grep -c '^processor' /proc/cpuinfo) install
-make clean
